f03499d748fe5a661ee8c9ed87513e3dce5ae0b7
Gustavo Meireles <gustavo.meireles@gmail.com>
2020-03-19 00:24:06 -0300

    wip

--- src/HasScopeChecks.php
+++ src/HasScopeChecks.php
@@ -11,9 +11,14 @@ trait HasScopeChecks
         return Str::startsWith($method, 'is') || Str::startsWith($method, 'has');
     }
 
+    protected function isCheckScopeNegationMethod(string $method): bool
+    {
+        return Str::startsWith($method, 'isNot') || Str::startsWith($method, 'hasNo');
+    }
+
     protected function getOriginalScopeMethodName($checkMethodName): string
     {
-        return preg_replace('/^(is|has)(.*)$/m', '$2', $checkMethodName);
+        return preg_replace('/^(isNot|hasNo|is|has)(.*)$/m', '$2', $checkMethodName);
     }
 
     protected function forwardCallTo($object, $method, $parameters)
@@ -25,6 +30,10 @@ trait HasScopeChecks
                 $builder = $this->newQuery()->where($this->getKeyName(), $this->getKey());
 
                 $builder = call_user_func_array([$builder, $originalScopeMethodName], $parameters);
+
+                if ($this->isCheckScopeNegationMethod($method)) {
+                    return ! $builder->exists();
+                }
 
                 return $builder->exists();
             }
